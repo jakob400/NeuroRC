fa1875a7c378dfe8bdf27a299e682bac8430d491
jakob400 <jakob.400@gmail.com>
2026-05-10 23:50:04 -0400

    BIO-5: Honor conductance_*_init constants in graph_build
    set_graph_attributes zeroed g_A/g_E/g_I instead of pulling
    conductance_A_init/E_init/I_init from const.py, forcing a 50-100 ms
    relaxation transient at the start of every STR run before conductances
    reached their nominal steady state. Use the declared init values so
    the first timestep already sits near the working point.

--- graph_build.py
+++ graph_build.py
@@ -15,16 +15,15 @@ def set_graph_attributes(G,model):
 	"""Sets initial graph parameters according to model. LIF only requires voltage update while STR requires conducdtance updates."""
 	#timelist = [0] * const.timesteps # makes 0-list of size 'timesteps'
 	v0 = const.voltage_init # Importing initial voltage parameter.
+	gA0 = const.conductance_A_init
+	gE0 = const.conductance_E_init
+	gI0 = const.conductance_I_init
 	if (model == 'STR'):
 		for j in range(G.number_of_nodes()): # Iterating over all nodes, to set initial parameters.
-			timelistv = [v0] # Doing this step separately for readability.
-			timelistcA = [0]
-			timelistcE = [0]
-			timelistcI = [0]
-			G.nodes[j].update({'voltage' : timelistv})
-			G.nodes[j].update({'conductance_A' : timelistcA})
-			G.nodes[j].update({'conductance_E' : timelistcE})
-			G.nodes[j].update({'conductance_I' : timelistcI})
+			G.nodes[j].update({'voltage' : [v0]})
+			G.nodes[j].update({'conductance_A' : [gA0]})
+			G.nodes[j].update({'conductance_E' : [gE0]})
+			G.nodes[j].update({'conductance_I' : [gI0]})
 	if (model == 'LIF'):
 		for j in range(G.number_of_nodes()): # Iterating over all nodes, to set initial parameters.
 			timelistv = [v0]
